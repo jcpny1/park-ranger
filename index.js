1f5ffd1245fd302535d20226d013922759f7f131
Mark Hendrickson <markmhendrickson@gmail.com>
2017-03-29 17:23:24 +0200

    Add support for returned variables object
    - Improve comments
    - Integrate path package for path resolution
    - Order package.json

--- index.js
+++ index.js
@@ -1,16 +1,28 @@
 /**
- * Load environment variables with dotenv, environment name and config file in base directory.
+ * Load into process.env or return environment variables stored in env file.
  * @module
  */
 
 var dotenv = require('dotenv');
 var fs = require('fs');
+var Path = require('path');
 
-module.exports = (name) => {
+/** 
+ * Load or return environment variables
+ * @param {string} [name] - Name of environment. Used to suffix env file with preceding dash (e.g. ".env-test"). If null, file with name ".env" will be used
+ * @param {boolean} [returnObject] - Whether to return an object with the variables instead of applying to process.env
+ */
+module.exports = (name, returnObject) => {
   var suffix = name ? '-' + name : '';
-  var path = `${__dirname}/../../.env${suffix}`;
+  var path = Path.resolve(__dirname, '../../.env' + suffix);
 
-  if (fs.existsSync(path)) {
+  if (!fs.existsSync(path)) {
+    throw new Error(`Environment variables file does not exist at path: ${path}`);
+  }
+
+  if (returnObject) {
+    return dotenv.parse(fs.readFileSync(path));
+  } else {
     dotenv.config({ path: path });
   }
 };
